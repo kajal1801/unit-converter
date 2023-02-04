94b529dc16c3f9ad0ad67cf3fe61f6b370d1b291
kajal1801 <anshikajais2001@gmail.com>
2023-02-04 13:54:43 +0530

    Added an error message for string inputs

--- index.js
+++ index.js
@@ -11,26 +11,41 @@ const volumeEl = document.getElementById("volume-el")
 const massEl = document.getElementById("mass-el")
 const resetBtn = document.getElementById("reset-btn")
 
- convertBtn.addEventListener("click", function(){
-     let num = inputEl.value
-     renderValues(num)
- })
- 
- function renderValues(num){
-     let meter = (num / 3.281).toFixed(2)
-     let feet = (num * 3.281).toFixed(2)
-     let liters = (num / 0.264).toFixed(2)
-     let gallons = (num * 0.264).toFixed(2)
-     let kg = (num / 2.204).toFixed(2)
-     let pound = (num * 2.204).toFixed(2)
-     
-     lengthEl.innerHTML = `${num} meters = ${feet} feet | ${num} feet = ${meter} meters`
-     volumeEl.innerHTML = `${num} liters = ${gallons} gallons | ${num} galoons = ${liters} liters`
-     massEl.innerHTML = `${num} kilograms = ${pound} pounds | ${num} pounds = ${kg} kilograms`
- }
- 
- 
-resetBtn.addEventListener("click", function(){
-    inputEl.value = ""
-    lengthEl.innerHTML = volumeEl.innerHTML = massEl.innerHTML = "Enter a value in the input box"
+convertBtn.addEventListener("click", function () {
+    let num = inputEl.value;
+    if (typeof num === "string") {
+        errorMessage()
+    }
+    else {
+        renderValues(num)
+    }
+})
+
+function renderValues(num) {
+    let meter = (num / 3.281).toFixed(2)
+    let feet = (num * 3.281).toFixed(2)
+    let liters = (num / 0.264).toFixed(2)
+    let gallons = (num * 0.264).toFixed(2)
+    let kg = (num / 2.204).toFixed(2)
+    let pound = (num * 2.204).toFixed(2)
+
+    lengthEl.innerHTML = `${num} meters = ${feet} feet | ${num} feet = ${meter} meters`
+    volumeEl.innerHTML = `${num} liters = ${gallons} gallons | ${num} galoons = ${liters} liters`
+    massEl.innerHTML = `${num} kilograms = ${pound} pounds | ${num} pounds = ${kg} kilograms`
+}
+
+function errorMessage() {
+    inputEl.value = "";
+    lengthEl.innerHTML =
+        volumeEl.innerHTML =
+        massEl.innerHTML =
+        "Please enter a valid number"
+}
+
+resetBtn.addEventListener("click", function () {
+    inputEl.value = "";
+    lengthEl.innerHTML =
+        volumeEl.innerHTML =
+        massEl.innerHTML =
+        "Enter a value in the input box"
 })
